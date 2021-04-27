ea764d482542687abcd2af0b6c4aa8c7d5e1ecfd
RobotIO51 <michael.sirota02@gmail.com>
2021-04-27 20:20:06 +0300

    end

--- Tragil1.py
+++ Tragil1.py
@@ -1,1 +1,7 @@
-print("hello")
+if __name__ == '__main__':
+    count = 0
+    number = input()
+    while number != "stop":
+        count = count + int(number)
+        number = input()
+    print(count)
